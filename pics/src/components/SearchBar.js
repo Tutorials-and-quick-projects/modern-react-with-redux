ce26848b23fa1eb2d709e6fdf19d700f55f12741
jdraper9 <jamesdraper24@gmail.com>
2020-02-02 09:14:14 -0800

    initial commit for pics project - search images with api

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div>
+            <label>Image Search</label>
+            <input type="text" onChange={this.onInputChange} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
+
+// event handlers:
+// onClick
+// onChange
+// onSubmit
+// no () in event handler
