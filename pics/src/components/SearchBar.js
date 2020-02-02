4e093c9a3d3169ab113c2313dc2bf00bee7f5c52
jdraper9 <jamesdraper24@gmail.com>
2020-02-02 13:50:18 -0800

    make api call to unsplash using axios. Also explore usage of this in react, and how to clean up api call from App component

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  // class has properties
+  // this gives access to properties of instance of class belonging to
+
+  state = { term: '' };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div>
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
@@ -26,3 +37,15 @@ export default SearchBar;
 // onChange
 // onSubmit
 // no () in event handler
+
+// onChange={e => console.log(e.target.value)}
+
+// controlled: give react world info via state, instead of reaching into DOM to see value via value={this.state.term}
+// dont store data in html!
+// centralize data inside of React 5
+
+// arrow functions automatically bind this (of function def)
+// or can in constructor write
+// constructor() {
+//   this.drive = this.drive.bind(this);
+// }
